2f2d1298050a65e2f13e66d5a6d84e7c92ad8080
Double Yoo <doubleyoo@Doubles-MBP.lan1>
2021-04-21 11:15:16 -0400

    updating HW9

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
+const inquirer = require("inquirer");
+const path = require('path');
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown.js')
+
 // TODO: Include packages needed for this application
 
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub user name?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project here:'
+
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "What kind of license should your project have?",
+        choices: ['MIT', 'Apache', 'GPL']
+    },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(answers)
+
+        })
+
+}
 
 // Function call to initialize app
 init();
