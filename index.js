7faa6e95eabb0ce83079e2119c2f08c7e8949d92
Double Yoo <doubleyoo@Doubles-MacBook-Pro.local>
2021-05-18 12:08:24 -0400

    updating index.js

--- index.js
+++ index.js
@@ -6,18 +6,7 @@ const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Include packages needed for this application
 
 
-// TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is your GitHub user name?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?'
-    },
     {
         type: 'input',
         name: 'title',
@@ -31,22 +20,88 @@ const questions = [
 
     },
     {
+        type: 'input',
+        name: 'installation',
+        message: 'How is your software installed?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How is your software used?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How is your software used?'
+    },
+    {
         type: 'list',
         name: 'license',
-        message: "What kind of license should your project have?",
+        message: "What kind of license does your project have?",
         choices: ['MIT', 'Apache', 'GPL']
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'Where can people contribute to this project?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How can a user test your software?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub user name?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
     },
 
 ];
 
+
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(input) {
+    var readMeText = `# ${input.title}
+##Table of Contents 
+[Description](#description)
+[Installation](#installation)
+[Usage](#usage)
+[License](#license)
+[Contributing](#contributing)
+[Testing](#testing)
+[Questions](#questions)
+
+## Description 
+${input.description}
+## Installation 
+${input.installation}
+## Usage
+${input.usage}
+## License
+${input.license}
+## Contributing
+${input.contributions}
+## Testing
+${input.test}
+## Questions 
+(${input.github})
+${input.email}`
+
+    console.log(readMeText)
+
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
         .then((answers) => {
-            console.log(answers)
+            writeToFile(answers)
 
         })
 
